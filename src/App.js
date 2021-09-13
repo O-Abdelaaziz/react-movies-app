39b0ca709137d06bd01fe9d182ccaf6fa080cac1
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-09-13 22:46:54 +0200

    Task 3: handle movies list & delete event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
+import Movies from './components/Movies';
 
 function App() {
   return (
-    <main role="main" class="container">
-
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br /> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-
+    <main className="container">
+      <Movies />
     </main>
   );
 }
---
+++ src/components/Movies.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+const Movies = () => {
+  const [movies, setMovies] = useState(getMovies());
+
+  const deleteMovieHandler = (id) => {
+    setMovies((prevMovie) => {
+      const updateMovies = prevMovie.filter((movie) => movie._id !== id);
+      return updateMovies;
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <div className="alert alert-primary mt-2" role="alert">
+        Showing {movies.length} movies in the database.
+      </div>
+      {movies.length === 0 && <p>There are no movies in the database</p>}
+      {movies.length > 0 && (
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie._id}>
+                <th scope="row">{movie.title}</th>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => deleteMovieHandler(movie._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Movies;
